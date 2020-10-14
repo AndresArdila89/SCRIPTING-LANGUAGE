6c7d2da21f333680b025310441f6c550b63af8a1
AndresArdila89 <andresardila@me.com>
2020-10-14 12:11:34 -0400

    missing last 30 mintus

--- Class_7/book.py
+++ Class_7/book.py
@@ -72,23 +72,3 @@ class Book:
     
     
 ##================================Display 
-b1 = Book("Red Rose","Walter Abish","Marc London", 169.99)
-print(b1)
-print(b1.title)
-print(b1.author)
-print(b1.editor)
-print(b1.price)
-print(b1.bookId)
-
-print("================================================")
-
-b2 = Book("Davinci","some body","Langdon", 99.99)
-print(b2)
-print(b2.getTitle)
-print(b2.getAuthor)
-print(b2.getEditor)
-b2.setPrice = 100
-print(b2.getPrice)
-print(b2.getBookID)
-
-print("================================================")
