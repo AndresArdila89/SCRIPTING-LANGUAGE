baab9e756bf1fe07927c7a733daba262683e2311
AndresArdila89 <andresardila@me.com>
2020-10-16 08:15:08 -0400

    q3

--- assignment_3/Q3.py
+++ assignment_3/Q3.py
@@ -3,3 +3,18 @@
     We’ll say that a String s1 and s2 is balanced if all the chars in the string1 are there in s2.
     characters position doesn’t matter.
 '''
+
+s1 = "nyf"
+s2 = "Pynative"
+x = True
+
+
+for letter in s1:
+    if not letter in s2:
+        x = False
+        break
+    else:
+        pass
+
+
+print(x)
